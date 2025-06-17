574435271822b6c3b105b443df62bbb639c27078
Evander Álvarez <78768993+Evander-byte@users.noreply.github.com>
2025-06-17 15:29:19 -0600

    Local configuration of DB

--- src/config/db/db.ts
+++ src/config/db/db.ts
@@ -3,11 +3,12 @@ import dotenv from "dotenv"
 
 dotenv.config()
 
-export const db = new Sequelize(process.env.DATABASE_URL, {
-  models: [__dirname + "../../../models/**/*"],
-  dialectOptions: {
-    ssl: {
-      require: false
-    }
-  }
+export const db = new Sequelize(
+  process.env.DATABASE_NAME, 
+  process.env.DATABASE_USERNAME,
+  process.env.DATABASE_PASSWORD, {
+  host: process.env.DATABASE_HOST,
+  dialect: "postgres",
+  port: 5432,
+  models: [__dirname + "../../../models/**/*"]
 })
