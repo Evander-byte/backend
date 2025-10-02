2dd697260eb8d7e65fe26f781d05267f80294efd
Evander Álvarez <78768993+Evander-byte@users.noreply.github.com>
2025-10-02 16:51:09 -0600

    Performe middleware

--- src/middleware/budget.ts
+++ src/middleware/budget.ts
@@ -5,52 +5,70 @@ import Budget from "../models/Budget";
 declare global {
   namespace Express {
     interface Request {
-      budget?: Budget
+      budget?: Budget;
     }
   }
 }
 
-export const validateBudgetId = async (req: Request, res: Response, next: NextFunction) => {
-  await param('budgetId').isInt().withMessage("Invalid ID")
-  .custom(value => value > 0).withMessage("Invalid ID")
-  .run(req)
-  next()
+export const validateBudgetId = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  await param("budgetId")
+    .isInt()
+    .withMessage("Invalid ID")
+    .custom((value) => value > 0)
+    .withMessage("Invalid ID")
+    .run(req);
+  next();
+};
 
-}
-
-export const validateBudgetExists = async (req: Request, res: Response, next: NextFunction) => {
+export const validateBudgetExists = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
-    const {budgetId} = req.params
-    const budget = await Budget.findByPk(budgetId)
-    if(!budget) {
-      const error = new Error("Budget not found")
-      res.status(404).json({error: error.message})
+    const { budgetId } = req.params;
+    const budget = await Budget.findByPk(budgetId);
+    if (!budget) {
+      const error = new Error("Budget not found");
+      res.status(404).json({ error: error.message });
     }
-    req.budget = budget
-    next()
+    req.budget = budget;
+    next();
   } catch (error) {
-    res.status(500).json({error: "There was an error"})
+    res.status(500).json({ error: "Invalid ID" });
   }
-}
+};
 
-export const validateBudgetInput = async (req: Request, res: Response, next: NextFunction) => {
-    await body("name")
-      .notEmpty().withMessage("Budget's name cannot be empty")
-      .run(req)
-      
-    await body("amount")
-      .notEmpty().withMessage("Budget's amount cannot be empty")
-      .isNumeric().withMessage("Invalid amount")
-      .custom(value => value > 0).withMessage("Budget's amount must be higher than zero ")
-      .run(req)
-      next()
-}
+export const validateBudgetInput = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  await body("name")
+    .notEmpty()
+    .withMessage("Budget's name cannot be empty")
+    .run(req);
+
+  await body("amount")
+    .notEmpty()
+    .withMessage("Budget's amount cannot be empty")
+    .isNumeric()
+    .withMessage("Invalid amount")
+    .custom((value) => value > 0)
+    .withMessage("Budget's amount must be higher than zero ")
+    .run(req);
+  next();
+};
 
 export const hasAccess = (req: Request, res: Response, next: NextFunction) => {
-  if(req.budget.userId !== req.user.id) {
-    const error = new Error("Invalid action")
-    res.status(401).json({error: error.message})
-    return
+  if (req.budget.userId !== req.user.id) {
+    const error = new Error("Invalid action");
+    res.status(401).json({ error: error.message });
+    return;
   }
-  next()
-}
+  next();
+};
