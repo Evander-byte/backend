deaacf79efcbe90285ca7ccdecac8a29912d3004
Evander Álvarez <78768993+Evander-byte@users.noreply.github.com>
2025-10-09 12:34:13 -0600

    Change message property in createExpense

--- src/controllers/ExpenseController.ts
+++ src/controllers/ExpenseController.ts
@@ -1,30 +1,28 @@
-import type { Request, Response } from 'express';
-import Expense from '../models/Expense';
+import type { Request, Response } from "express";
+import Expense from "../models/Expense";
 
 export class ExpenseController {
-  static getAll = async (req: Request, res: Response,) => {
-
-  }
-  static create = async (req: Request, res: Response,) => {
+  static getAll = async (req: Request, res: Response) => {};
+  static create = async (req: Request, res: Response) => {
     try {
-      const expense = await Expense.create(req.body)
-      expense.budgetId = req.budget.id
-      await expense.save()
-      res.status(201).json({message: "Expense added successfully"})
+      const expense = await Expense.create(req.body);
+      expense.budgetId = req.budget.id;
+      await expense.save();
+      res.status(201).json("Expense added successfully");
     } catch (error) {
-      res.status(500).json({error: "There was an error"})
+      res.status(500).json({ message: "There was an error" });
     }
-  }
-  static getById = async (req: Request, res: Response,) => {
-    const expense = await Expense.findByPk(req.expense.id)
-    res.status(200).json(expense)
-  }
-  static updateById = async (req: Request, res: Response,) => {
-    await req.expense.update(req.body)
-    res.status(201).json({message: "Expense updated successfully"})
-  }
-  static deleteById = async (req: Request, res: Response,) => {
-    await req.expense.destroy()
-    res.status(200).json({message: "Expense deleted successfully"})
-  }
+  };
+  static getById = async (req: Request, res: Response) => {
+    const expense = await Expense.findByPk(req.expense.id);
+    res.status(200).json(expense);
+  };
+  static updateById = async (req: Request, res: Response) => {
+    await req.expense.update(req.body);
+    res.status(201).json("Expense updated successfully");
+  };
+  static deleteById = async (req: Request, res: Response) => {
+    await req.expense.destroy();
+    res.status(200).json("Expense deleted successfully");
+  };
 }
